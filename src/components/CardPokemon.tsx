8872773d19f724d5241dff23a176c8851755f6ef
Arnaiz23 <aaf110501@gmail.com>
2023-02-11 15:38:46 +0100

    feat: 🎸 In process with the creation of the pokemon card

--- src/components/CardPokemon.tsx
+++ src/components/CardPokemon.tsx
@@ -1,11 +1,43 @@
 import { useEffect, useState } from "react"
+import pokemonResult from "@/mocks/pokemon-result.json"
 
-const CardPokemon = () => {
+interface Props {
+  url: string
+}
+
+const backgroundType = [
+  {
+    type: "fire",
+    bg: "#ff962f",
+    button: "#d87724",
+  },
+  {
+    type: "poison",
+    bg: "#d1688b",
+    button: "#a7526f",
+  },
+  {
+    type: "bug",
+    bg: "#c1bc0f",
+    button: "#999608",
+  },
+  {
+    type: "grass",
+    bg: "#52c458",
+    button: "#28a739",
+  },
+]
+
+const CardPokemon = ({ url }: Props) => {
   const [pokemon, setPokemon] = useState()
   const [isMounted, setIsMounted] = useState(false)
+  //const pokemon = pokemonResult
+
+  const urlImage = (name: string) =>
+    `https://img.pokemondb.net/artwork/large/${name}.jpg`
 
   const fetchData = async () => {
-    const response = await fetch(`${import.meta.env.VITE_API_URL}pokemon/1/`)
+    const response = await fetch(url)
     const data = await response.json()
     setPokemon(data)
     setIsMounted(true)
@@ -19,18 +51,43 @@ const CardPokemon = () => {
     return <h1>No pokemon</h1>
   }
 
+  const getBackground = (nameType, prop) => {
+    const typeBg = backgroundType.filter(({ type }) => type === nameType)
+    return typeBg.length > 0 ? typeBg[0][prop] : backgroundType[1].bg
+  }
   return (
-    <div className="w-52 rounded-md bg-[#d1688b] flex flex-col justify-center items-center">
-      <img
-        src={pokemon?.sprites?.front_default}
-        alt={`front of ${pokemon?.name}`}
-        className="w-20 -translate-y-10 bg-gray-50/50 block rounded-full"
-      />
-      <span className="bg-white/50 w-max flex p-1 px-3 rounded-2xl">
-        <p>#001</p>
+    <div
+      className={`w-52 rounded-md  relative flex flex-col justify-evenly items-center py-3 min-h-[300px]`}
+      style={{
+        backgroundColor: getBackground(pokemon.types[0].type.name, "bg"),
+      }}
+    >
+      <picture className="w-24 overflow-hidden h-24 absolute -top-[43px] rounded-full flex items-center">
+        <img
+          src={urlImage(pokemon?.name)}
+          alt={`front of ${pokemon?.name}`}
+          className="w-full object-cover  bg-gray-50/50 block"
+        />
+      </picture>
+      <span className="bg-white/50 w-max flex p-1 px-3 rounded-2xl mt-8">
+        <p>#{pokemon?.id}</p>
       </span>
-      <h2 className="capitalize">{pokemon?.name}</h2>
-      <h2>Si</h2>
+      <h2 className="capitalize text-2xl text-gray-700 font-bold">
+        {pokemon?.name}
+      </h2>
+      <div className="flex justify-center items-center gap-4">
+        {pokemon?.types.map((type) => (
+          <p>{type.type.name}</p>
+        ))}
+      </div>
+      <button
+        className="rounded-full p-1 px-3 text-white text-sm shadow shadow-black"
+        style={{
+          backgroundColor: getBackground(pokemon.types[0].type.name, "button"),
+        }}
+      >
+        Ver Gráfica
+      </button>
     </div>
   )
 }
