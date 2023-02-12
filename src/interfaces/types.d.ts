3a184dcae7d727dd23b73a8c876ea6b5fc7c6dfa
Arnaiz23 <aaf110501@gmail.com>
2023-02-12 17:23:38 +0100

    feat: 🎸 Create the interface of the pokemon

---
+++ src/interfaces/types.d.ts
@@ -0,0 +1,38 @@
+interface NameType {
+  name: string
+  url: string
+}
+
+interface Types {
+  slot: number
+  type: NameType
+}
+
+interface Move {
+  name: string
+  url: string
+}
+
+interface Moves {
+  move: Move
+}
+
+interface Stat {
+  name: string
+}
+
+interface Stats {
+  base_stat: number
+  effort: number
+  stat: Stat
+}
+
+export interface PokemonObject {
+  name: string
+  id: number
+  types: Types[]
+  moves: Moves[]
+  height: number
+  stats: Stats[]
+  weight: number
+}
--- src/pages/OnePokemon.tsx
+++ src/pages/OnePokemon.tsx
@@ -1,5 +1,8 @@
 import { useParams } from "react-router-dom"
 
+import { PokemonObject } from "@/interfaces/types"
+import pokemonJson from "@/mocks/pokemon-result.json"
+
 type PokemonParams = {
   name: string
 }
@@ -7,5 +10,53 @@ type PokemonParams = {
 export default function OnePokemonPage() {
   const { name } = useParams<PokemonParams>()
 
-  return <h1 className="text-white text-3xl">Pokemon {name}</h1>
+  const urlImage = `https://img.pokemondb.net/artwork/large/${name}.jpg`
+
+  const pokemon: PokemonObject = {
+    name: pokemonJson.name,
+    id: pokemonJson.id,
+    types: pokemonJson.types,
+    moves: pokemonJson.moves,
+    height: pokemonJson.height,
+    stats: pokemonJson.stats,
+    weight: pokemonJson.weight,
+  }
+
+  return (
+    <>
+      <h1 className="text-white text-3xl">Pokemon {pokemon.name}</h1>
+      <p className="text-white">id: {pokemon.id}</p>
+      <p className="text-white">height: {pokemon.height * 10} cm</p>
+      <p className="text-white">weight: {(pokemon.weight * 100) / 1000} kg</p>
+      <div className="flex justify-evenly items-center gap-10">
+        <p className="text-white">Types:</p>
+        <ul className="text-white">
+          {pokemon.types.map((type) => (
+            <li className="list-disc">{type.type.name}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="flex justify-evenly items-center gap-10">
+        <p className="text-white">Moves:</p>
+        <ul className="text-white">
+          {pokemon.moves.slice(0, 4).map((move) => (
+            <li className="list-decimal">{move.move.name}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="flex justify-evenly items-center gap-10">
+        <p className="text-white">Stats:</p>
+        <ul>
+          {pokemon.stats.map((stat) => (
+            <li className="text-white">
+              {stat.stat.name}:{stat.base_stat}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <picture>
+        <img src={urlImage} alt={`pokemon ${pokemon.name}`} />
+      </picture>
+    </>
+  )
 }
