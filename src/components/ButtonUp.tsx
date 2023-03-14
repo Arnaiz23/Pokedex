bfda0b2540c7cca030b52988a76bf53f39ec55c2
Arnaiz23 <aaf110501@gmail.com>
2023-03-14 21:22:07 +0100

    feat: 🎸 Add an arrow svg to the upbutton

--- src/components/ButtonUp.tsx
+++ src/components/ButtonUp.tsx
@@ -1,7 +1,22 @@
+import UpArrowIcon from "./Icons/UpArrow"
+
 export default function ButtonUp() {
+  const goUp = () => {
+    var currentScroll = document.documentElement.scrollTop
+
+    if (currentScroll > 0) {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      })
+    }
+  }
+
   return (
     <span className="fixed bottom-2 right-2 p-3 dark:bg-blue-500 bg-gray-700 rounded-full grid items-center dark:text-black text-white">
-      <button>Up</button>
+      <button onClick={goUp}>
+        <UpArrowIcon />
+      </button>
     </span>
   )
 }
