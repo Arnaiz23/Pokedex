bf31d3755d76012083f2b0603463ff6d19187a4b
Arnaiz23 <aaf110501@gmail.com>
2023-03-22 14:38:38 +0100

    feat: 🎸 Disabled the searchBar for no endpoint pokeAPI

--- src/pages/AllPokemons.tsx
+++ src/pages/AllPokemons.tsx
@@ -2,7 +2,6 @@ import CardPokemon from "@/components/CardPokemon"
 import usePokemons from "@/hooks/usePokemons"
 import { PokemonArray } from "@/interfaces/types"
 import Loading from "@/components/Loading"
-import SearchBar from "@/components/SearchBar"
 
 interface PokemonsProps {
   pokemons: PokemonArray[]
@@ -11,7 +10,6 @@ interface PokemonsProps {
 function Pokemons({ pokemons }: PokemonsProps) {
   return (
     <main className="w-full flex justify-center items-center flex-col gap-12">
-      <SearchBar />
       <div className="gap-y-14 gap-x-5 grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] place-items-center w-[80%] mx-auto">
         {pokemons.map(({ name }) => (
           <CardPokemon key={name} name={name} />
