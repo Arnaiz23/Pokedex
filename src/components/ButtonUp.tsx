9f222a6e28ed5d653f064d910d70251802375623
Arnaiz23 <aaf110501@gmail.com>
2023-04-11 13:04:55 +0200

    feat: 🎸 Update the buttonUp

--- src/components/ButtonUp.tsx
+++ src/components/ButtonUp.tsx
@@ -1,8 +1,11 @@
+import { useEffect, useState } from "react"
 import UpArrowIcon from "./Icons/UpArrow"
 
 export default function ButtonUp() {
+  const [showButton, setShowButton] = useState(false)
+
   const goUp = () => {
-    var currentScroll = document.documentElement.scrollTop
+    const currentScroll = document.documentElement.scrollTop
 
     if (currentScroll > 0) {
       window.scrollTo({
@@ -12,8 +15,24 @@ export default function ButtonUp() {
     }
   }
 
+  useEffect(() => {
+    const showButtonScroll = () => {
+      const currentScroll = document.documentElement.scrollTop
+
+      if(currentScroll > 300) {
+	setShowButton(true)
+      } else {
+	setShowButton(false)
+      }
+    }
+
+    window.addEventListener("scroll", showButtonScroll)
+
+    return () => window.removeEventListener("scroll", showButtonScroll)
+  }, [])
+
   return (
-    <span className="fixed bottom-2 right-2 p-3 dark:bg-blue-500 bg-gray-700 rounded-full grid items-center dark:text-black text-white">
+    <span className={`fixed bottom-2 right-2 p-3 dark:bg-blue-500 bg-gray-700 rounded-full grid items-center dark:text-black text-white ${showButton ? `scale-100` : `scale-0`} transition-transform`}>
       <button onClick={goUp}>
         <UpArrowIcon />
       </button>
